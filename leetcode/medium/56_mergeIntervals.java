8aeab73ae3c458bc44bf78f4d66178735cd3891b
Ravi Kumar Yadav <ravi.yadav@paytm.com>
2026-04-17 17:34:57 +0530

    Medium | Ravi | solved 56-MergeIntervals

---
+++ leetcode/medium/56_mergeIntervals.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+	public int[][] merge(int[][] intervals){
+
+		List<int[]> result = new ArrayList<>();
+
+		// sort the intervals by start time
+		Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+		// loop over intervals and merge the overlapping intervals into list
+		for (int[] interval: intervals) {
+			if (result.size() == 0){
+				result.add(interval);
+			} else if (result.get(result.size()-1)[1] < interval[0]) {
+				result.add(interval);
+			} else {
+				result.get(result.size()-1)[1] = Math.max(result.get(result.size()-1)[1], interval[1]);
+			}
+		}
+
+		// convert list to array and return
+		return result.toArray(new int[0][]);
+	}
+}
